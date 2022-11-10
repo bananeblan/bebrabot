f47db20276f62ba334509dc46029c740c0f13fb4
bananeblan <117469313+bananeblan@users.noreply.github.com>
2022-11-10 23:36:59 +0500

    Update bbot.py

--- bbot.py
+++ bbot.py
@@ -1,4 +1,5 @@
 import telebot
+from random import randint
 from telebot import types
 
 bot = telebot.TeleBot('5330503333:AAFGPo7lVaZVTZY7nEO2xWkfHkepbN5VdDY')
@@ -28,6 +29,18 @@ def get_text_messages(message):
     elif "бот" in message.text:
         if "иди нахуй" in message.text:
             bot.send_message(message.chat.id, 'Сам иди нахуй, чурка')
+        elif "или" in message.text:
+            list = message.text.split()
+            a = ""
+            b = ""
+            for i in range(len(list)):
+                if list[i] == "или":
+                    a = list[i-1]
+                    b = list[i+1]
+            if randint(0,1) == 0:
+                bot.send_message(message.chat.id, a)
+            else:
+                bot.send_message(message.chat.id, b)
     elif message.text == 'автоферма в вк':
         bot.send_message(message.chat.id, 'vk.com/avtoferma')
     elif message.text == 'автоферма в телеграме!!!!':
